3c55222a249938297a75f7c4d408f448969ada39
Coruja <wlodarczyk.mateusz@gmail.com>
2014-11-09 21:44:57 +0100

    SPI configuration was added

--- spi/include/spi.h
+++ spi/include/spi.h
@@ -1,33 +1,78 @@
 #ifndef SPI_H
 #define SPI_H
 
-typedef enum 
-{
-	USI_SPI_MODE0,
-	USI_SPI_MODE1
-} USI_SPI_Mode_t;
+/* Pin configuration for Attiny 2313 */
+#define USI_SPI_MISO _BV(PB5)
+#define USI_SPI_MOSI _BV(PB6)
+#define USI_SPI_USCK _BV(PB7)
 
-typedef enum
-{
-	USI_SPI_SS_INT0,
-	USI_SPI_SS_INT1,
-	USI_SPI_SS_PCINT
-} USI_SPI_SS_t;
+/*
+ * Uncomment define USI_SPI_SLAVE_MODE to enable SPI slave mode.
+ * By default driver works as Master.
+ */
+#define USI_SPI_SLAVE_MODE
 
-typedef struct
-{
-	USI_SPI_Mode_t mode;
-	unsigned char isSlave; /* 0 - Master, 1 - Slave */
-	unsigned char isNCS;   /* if 1 then transistion of SS pin from high to low enables transmission */
-	USI_SPI_SS_t ssPin;    /* pin used as Slave Select */
-	unsigned char pcintN;  /* 0..7 - number of pin if USI_SPI_SS_PCINT is used as ssPin */
-} USI_SPI_Config_t;
+/*
+ * This is interrutpt driven implementation, so interrupt pin is
+ * needed to notify uC about transmission.
+ * To do that a Slave Select Pin needs to be configured.
+ * Uncomment ONE of following defines.
+ */
+#define USI_SPI_SS_INT0
+//#define USI_SPI_SS_INT1
+//#define USI_SPI_SS_PCINT
+
+/*
+ * When using USI_SPI_SS_PCINT as SS pin you need to define 
+ * Only values from 0 to 7 are valid, but may noticed, that
+ * PICNT pins overlaps with SPI pins (DO, DI, USCK), so in
+ * fact only 0-4 pins can be used.
+ */
+//#define USI_SPI_SS_PCINT_N 0
+
+/*
+ * When using Slave mode.
+ * You can change behavior of SS pin to work as Not Select Cable. 
+ * So HIGH state of SS pin will indicate that uC is NOT selected,
+ * whereas LOW state will indicate uC is selected.
+ * To enable this feature uncomment following define.
+ */
+//#define USI_SPI_USE_NCS
+
+/*
+ * Uncommoment following define to enablr SPI Mode 1, a.k.a (0,1).
+ * It works for both Master and Slave configuration.
+ */
+//#define USI_SPI_MODE1
+
+
+#if defined(USI_SPI_SLAVE_MODE)
+	#if !defined(USI_SPI_SS_INT0) && !defined(USI_SPI_SS_INT1) && !defined(USI_SPI_SS_PCINT)
+		#error Slave Select Pin wasnt configures: USI_SPI_SS_INT0, USI_SPI_SS_INT1, USI_SPI_SS_PCINT
+	#endif
+#endif
+
+#if defined(USI_SPI_SS_PCINT)
+	#if !defined(USI_SPI_SS_PCINT_N)
+		#error If USI_SPI_SS_PCINT is used than USI_SPI_SS_PCINT_N needs to be defined: 0..7
+	#endif
+#endif
+
+/*
+ * This is a callback declaration which has to be implemented 
+ * by user, when utilizing Slave mode.
+ * This callback will be invoked after every 8bit transmission.
+ * The parameter is 8bit data received thru SPI. It should 
+ * return 8bit that should be transmitted back to Master.
+ */
+unsigned char USI_SPI_callback( unsigned char data );
 
 /* Initialize interrupt driven SPI */
-void USI_SPI_Init( USI_SPI_Config_t* config );
+void USI_SPI_Init( void );
 
 unsigned char USI_SPI_TransferByte( unsigned char msg );
 
 unsigned char USI_SPI_Transfer( unsigned char* msg, unsigned int msg_size );
 
 #endif
+
--- spi/src/spi.c
+++ spi/src/spi.c
@@ -3,31 +3,65 @@
 #include <avr/interrupt.h>
 #include <spi.h>
 
-static unsigned char isSelected = 0;
-
-void USI_SPI_Init( USI_SPI_Config_t* config )
+static inline unsigned char isSelected()
 {
-	USICR = _BV(USIWM0) | _BV(USICS1);
+	unsigned char s;
+#if defined(USI_SPI_SS_INT0)
+	s = PIND & _BV(PD2);
+#elif defined(USI_SPI_SS_INT1)
+	s = PIND & _BV(PD3);
+#elif defined(USI_SPI_SS_PCINT)
+	s = PINB & ( 1 << USI_SPI_SS_PCINT_N );
+#endif 
 
-	MCUCR = _BV(ISC00);
-	GIMSK = _BV(INT0);
+#ifdef USI_SPI_USE_NCS
+	return 0 == s;
+#else
+	return 0 != s;
+#endif
+}
 
-	DDRB |=  _BV(PB6); //DO as output
-	DDRB &= ~_BV(PB5); //DI as input
-	DDRB &= ~_BV(PB7); //SCK as input
+void USI_SPI_Init( void )
+{
+#ifdef USI_SPI_SLAVE_MODE
 
-	DDRB |= _BV(PB2);
+	#if defined(USI_SPI_SS_INT0)
+		MCUCR = _BV(ISC00);
+		GIMSK = _BV(INT0);
+	#elif defined(USI_SPI_SS_INT1)
+		MCUCR = _BV(ISC10);
+		GIMSK = _BV(INT1);
+	#elif defined(USI_SPI_SS_PCINT)
+		GIMSK = _BV(PCIE);
+		PCMSK = ( 1 << USI_SPI_SS_PCINT_N );
+	#endif
+
+	USICR = _BV(USIWM0) | _BV(USICS1);  //enables SPI
+	DDRB |= USI_SPI_MOSI;  //DO as output
+	DDRB &= ~USI_SPI_MISO; //DI as input
+	DDRB &= ~USI_SPI_USCK; //SCK as input
+
+#else /* Master mode */
+	/*      three-wire,    ext clk,     use USITC */
+	USICR = _BV(USIWM0) | _BV(USICS1) | _BV(USICLK);
+	DDRB |= USI_SPI_MOSI;  //DO as output
+	DDRB |= USI_SPI_USCK;  //USCK as output
+	DDRB &= ~USI_SPI_MISO; //DI as input
+#endif
+
+#ifdef USI_SPI_MODE1
+	USICR |= _BV(USICS0);
+#endif
 }
 
 unsigned char USI_SPI_TransferByte( unsigned char msg )
 {
-    USISR = 0xf0;         // reset counter to 0 and clears all flags
+    USISR = 0xf0; // reset counter to 0 and clears all flags
     USIDR = msg;
 
     do
     {
-        /*      three-wire,    ext clk,     use USITC,    toggle USCK */
-        USICR = _BV(USIWM0) | _BV(USICS1) | _BV(USICLK) | _BV(USITC);
+        USICR |= _BV(USITC);
     } while( !( USISR & _BV(USIOIF) ) );
 
     return USIDR;
@@ -37,26 +71,28 @@ unsigned char USI_SPI_Transfer( unsigned char* msg, unsigned int msg_size )
 {
     do
     {
-        *msg = spi_transfer_byte( *msg );
+        *msg = USI_SPI_TransferByte( *msg );
         ++msg;
     } while (--msg_size);
 
     return 0;
 }
 
+#if defined(USI_SPI_SS_INT0)
 ISR( INT0_vect )
+#elif defined(USI_SPI_SS_INT1)
+ISR( INT1_vect )
+#elif defined(USI_SPI_SS_PCINT)
+ISR( PCINT_vect )
+#endif
 {
-	isSelected = PIND & _BV(PD2);
-		
-	if( isSelected )
+	if( isSelected() )
 	{
-		PORTB |= _BV(PB2);
 		USISR =  _BV(USIOIF); //Reset counter and overflow flag
 		USICR |= _BV(USIOIE); //Enable overflow interrupt
 	}
 	else
 	{
-		PORTB &= ~_BV(PB2);
 		USICR &= ~_BV(USIOIE); //Disable overflow interupt
 	}
 }
@@ -64,7 +100,8 @@ ISR( INT0_vect )
 ISR( USI_OVERFLOW_vect )
 {
 	unsigned char data = USIDR;
-	USIDR = ~data;
+	USIDR = USI_SPI_callback( data );
+//	USIDR = ~data;
 	USISR =  _BV(USIOIF); //reser interrupt flag
 }
 
