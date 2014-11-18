d75e28d0245d25e971e7fc77723458f381e4faa9
Coruja <wlodarczyk.mateusz@gmail.com>
2014-11-18 06:13:13 +0100

    test for master added

--- spi_test/src/main.c
+++ spi_test/src/main.c
@@ -7,12 +7,31 @@ unsigned char USI_SPI_callback( unsigned char data )
 	return ~data;
 }
 
+#define SS_PIN PD2
+#define NSS_HIGH { PORTD |= _BV(SS_PIN); }
+#define NSS_LOW  { PORTD &= ~_BV(SS_PIN); }
+
 int main( int argc, char* argv[] )
 {
 	USI_SPI_Init();
+	DDRD |= _BV(SS_PIN);
+    NSS_HIGH;
 	sei();
 
+#if USI_SPI_SLAVE_MODE
 	for(;;) _delay_ms(1000);
+#else
+	for( unsigned char i = 0 ;; i=(i>255?0:i+1) )
+	{
+		NSS_LOW;
+		_delay_ms(100);
+		//USI_SPI_Transfer( msg, 2 );
+		USI_SPI_TransferByte( i );
+		_delay_ms(100);
+		NSS_HIGH;
+		_delay_ms(900);
+	}
+#endif
 
 	return 0;
 }
