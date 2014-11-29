41edae0ee68de36647b57af92d6fea39ee590ee3
Coruja <wlodarczyk.mateusz@gmail.com>
2014-11-29 18:49:47 +0100

    UART read is enabled by default

--- uart/include/uart.h
+++ uart/include/uart.h
@@ -15,15 +15,11 @@
 #define UART_INVALID_CHAR 0x80
 #define UART_VALID_CHAR_MASK ( ~0x80 )
 
-#define DISABLE_READ
-
 /* If interrupt is meant to be used then sei() has to be invoked after init */
 void UART_Init( unsigned int baud );
 
 unsigned int UART_Transmit( const char* data );
 
-#ifndef DISABLE_READ
 unsigned int UART_Receive( char* data, unsigned int len );
-#endif
 
 #endif // UART_H_INCLUDED
--- uart/src/uart.c
+++ uart/src/uart.c
@@ -1,15 +1,11 @@
 #include <uart.h>
 
 #include <avr/io.h>
-#include <util/delay.h>
-#include <util/atomic.h>
 #include <avr/interrupt.h>
 #include "circbuffer.h"
 
 static CircBuffer_t gOutputCircBuffer;
-#ifndef DISABLE_READ
 static CircBuffer_t gInputCircBuffer;
-#endif
 
 void UART_Init( unsigned int baud )
 {
@@ -24,10 +20,8 @@ void UART_Init( unsigned int baud )
     UCSRC = ( 1 << UCSZ1 ) | ( 1 << UCSZ0 );
 
     /* Enable UART receive interrupt */
-#ifndef DISABLE_READ
     UCSRB |= (1 << RXCIE );
     CircBuffer_Init( &gInputCircBuffer );
-#endif
 
     CircBuffer_Init( &gOutputCircBuffer );
 }
@@ -44,7 +38,6 @@ unsigned int UART_Transmit( const char* data )
     return i;
 }
 
-#ifndef DISABLE_READ
 unsigned int UART_Receive( char* data, unsigned int len )
 {
     unsigned int i = 0;
@@ -53,7 +46,6 @@ unsigned int UART_Receive( char* data, unsigned int len )
     {
         if( !CircBuffer_IsEmpty( &gInputCircBuffer ) )
         {
-//            c = UART_ReadByte();
             CircBuffer_Read( &gInputCircBuffer, &c );
 #ifdef SKIP_INVALID_CHARS
             if( !( c & UART_VALID_CHAR_MASK ) )
@@ -70,17 +62,14 @@ unsigned int UART_Receive( char* data, unsigned int len )
 
     return i;
 }
-#endif
 
-#ifndef DISABLE_READ
 ISR( USART_RX_vect )
 {
     volatile unsigned char data = UDR;
     //TODO error check
     if( CIRCBUFFER_NOERROR != CircBuffer_Write( &gInputCircBuffer, data ) )
-        PORTB |= _BV(PB4);
+        ;
 }
-#endif
 
 ISR( USART_UDRE_vect )
 {
@@ -92,12 +81,3 @@ ISR( USART_UDRE_vect )
         UCSRB &= ~(1 << UDRIE);
 }
 
-#ifdef THIS_IS_WORKING
-/* left for reference */
-ISR( USART_RX_vect )
-{
-    unsigned char data = UDR;
-    while( !( UCSRA & ( 1 << UDRE ) ) ) ;
-    UDR = data;
-}
-#endif
